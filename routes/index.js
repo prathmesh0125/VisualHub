020504b5cab37858e099d66ca1cfa0bd26e462ea
prathmesh0125 <bidveprathmesh590@gmail.com>
2024-03-06 09:21:04 +0545

    authentication

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,62 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
+// const postModel = require("./post");
+const passport = require("passport");
+// const upload = require("./multer");
+const localStrategy = require("passport-local");
+passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index",);
+  // next()
 });
+router.get("/register", function (req, res, next) {
+  res.render("register", { title: "Express" });
+  // next()
+});
+router.get("/profile", function (req, res) {
+  res.render("profile");
+});
+
+
+router.post("/register", function (req, res, next) {
+  const data = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    contact: req.body.contact,
+  });
+  userModel.register(data, req.body.password).then(function () {
+    passport.authenticate('local')(req, res, function () {
+      res.redirect("/profile");
+    });
+  });
+});
+
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/",
+    successRedirect: "/profile",
+  }),
+  function (req, res, next) {}
+);
+
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/visulHub")
+  .then(console.log("database is connected succefully!"));
+
+// Define user schema
+const userSchema = new mongoose.Schema({
+  username: { type: String},
+  name: { type: String},
+  email: { type: String },
+  password: { type: String},
+  profileImages: String,
+  contact: Number,
+  boards: {
+    type: Array,
+    default: [],
+  },
 });
 
-module.exports = router;
+// Apply Passport-Local Mongoose plugin
+userSchema.plugin(plm);
+
+// Export user model
+module.exports = mongoose.model("User", userSchema);
